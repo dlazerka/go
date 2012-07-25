eff517278383c3657a520755c4e94f96eb1454f7
Dzmitry Lazerka <dlazerka@gmail.com>
2012-07-25 13:54:35 -0700

    Added GamesAPI.

--- src/src/com/dots/MainActivity.java
+++ src/src/com/dots/MainActivity.java
@@ -1,5 +1,10 @@
 package com.dots;
 
+import com.google.android.gms.common.ConnectionStatus;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.games.GamesClient;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,15 +14,40 @@ import android.view.Menu;
  */
 public class MainActivity extends Activity {
 
+  private GamesClient mGamesClient;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    String appId = getString(R.string.app_id);
+    mGamesClient = new GamesClient(
+        this,
+        appId,
+        new GamesAPIConnectionCallbacks(),
+        new GamesAPIOnConnectionFailedListener()
+    );
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+
+  private final class GamesAPIOnConnectionFailedListener implements OnConnectionFailedListener {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onConnectionFailed(ConnectionStatus arg0) {
+    }
+  }
+
+  private final class GamesAPIConnectionCallbacks implements ConnectionCallbacks {
+    @Override
+    public void onConnected() {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onDisconnected() {
     }
+  }
 }
