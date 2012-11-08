9cb39a41e509918a8385a8ae79d7e37935f8b4bb
Dzmitry Lazerka <dlazerka@gmail.com>
2012-11-07 23:46:04 -0800

    Added magic algo for placement of captured stones.

--- src/src/name/dlazerka/go/Util.java
+++ src/src/name/dlazerka/go/Util.java
@@ -3,4 +3,27 @@ package name.dlazerka.go;
 public class Util {
   public static final String TAG = "Dots";
 
+  /**
+   * Magic function for handsome placement of captured stones.
+   * Stones are placed in order:
+   * 0  2  6 12
+   * 1  3  7 13
+   * 4  5  8 14
+   * 9 10 11 15
+   * @return row * 1000 + col; all 0-based.
+   */
+  public static int getCapturedRowCol(int i) {
+    int row, col;
+    int rank = 0;
+    while (rank * rank <= i) rank++;
+    int d = rank * rank - i;
+    if (d <= rank) {// column
+      row = rank - d;
+      col = rank - 1;
+    } else {
+      row = rank - 1;
+      col = rank + rank - d - 1;
+    }
+    return row * 1000 + col;
+  }
 }
--- test/src/name/dlazerka/go/model/GameTest.java
+++ test/src/name/dlazerka/go/model/GameTest.java
@@ -1,12 +1,14 @@
 package name.dlazerka.go.model;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import name.dlazerka.go.model.Game;
-import name.dlazerka.go.model.Stone;
-import name.dlazerka.go.model.StoneColor;
+import java.util.HashSet;
+
+import name.dlazerka.go.GameArea;
+import name.dlazerka.go.Util;
 
 import org.junit.Test;
 
@@ -38,5 +40,60 @@ public class GameTest {
       return;
     }
     fail();
+  }
+
+  @Test
+  public void testGetCapturedPlacement0() {
+    assertEquals(0, Util.getCapturedRowCol(0));
+  }
+
+  @Test
+  public void testGetCapturedPlacement1() {
+    assertEquals(1000, Util.getCapturedRowCol(1));
+  }
+
+  @Test
+  public void testGetCapturedPlacement2() {
+    assertEquals(1, Util.getCapturedRowCol(2));
+  }
+
+  @Test
+  public void testGetCapturedPlacement3() {
+    assertEquals(1001, Util.getCapturedRowCol(3));
+  }
+
+  @Test
+  public void testGetCapturedPlacement4() {
+    assertEquals(2000, Util.getCapturedRowCol(4));
+  }
+
+  @Test
+  public void testGetCapturedPlacement5() {
+    assertEquals(2001, Util.getCapturedRowCol(5));
+  }
+
+  @Test
+  public void testGetCapturedPlacement6() {
+    assertEquals(2, Util.getCapturedRowCol(6));
+  }
+
+  @Test
+  public void testGetCapturedPlacement7() {
+    assertEquals(1002, Util.getCapturedRowCol(7));
+  }
+
+  @Test
+  public void testGetCapturedPlacement8() {
+    assertEquals(2002, Util.getCapturedRowCol(8));
+  }
+
+  @Test
+  public void testGetCapturedPlacement9() {
+    assertEquals(3000, Util.getCapturedRowCol(9));
+  }
+
+  @Test
+  public void testGetCapturedPlacement10() {
+    assertEquals(3001, Util.getCapturedRowCol(10));
   }
 }
